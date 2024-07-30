2fee50f390425e455671b570df8dddd4663a5b8e
Lenoch <81lennoch@gmail.com>
2024-07-30 15:18:12 +0200

    Enhance return structure for weather data responses
    - Added "code" key to return structure with appropriate HTTP status codes.
    - Modified existing returns to include nested "data" objects for consistency.
    - Improved error message structure to align with new format.

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ async def resume_weather(date: str = get_tomorrow_date()):
     if os.path.exists(file_path):
         with open(file_path, "r", encoding="utf-8") as file:
             try:
-                return json.load(file)
+                return {"code": 200, "data": json.load(file)}
             except json.decoder.JSONDecodeError:
                 pass
         os.remove(file_path)
         
     if date != get_tomorrow_date():
-        return {"text": "Error: Weather data for the requested date is not available"}
+        return {"code": 400, "data": {"text": "Error: Weather data for the requested date is not available"}}
     resume = ask_toobo.resume_weather()
     ask_toobo.save_resume(resume, date)
 
-    return {'text': resume}
+    return {"code": 200, "data": {"text": resume}}
 
 
 if __name__ == "__main__":
